e95a5144e72601595c9993a7dbb53f2e2841949c
boy16 <boy16@DESKTOP-CDTSI5B>
2017-04-11 16:46:03 +0900

    Test2

--- baseball_Simul_0.01/src/Main.java
+++ baseball_Simul_0.01/src/Main.java
@@ -4,6 +4,28 @@ public class Main {
 		
 		System.out.println("야구 시뮬레이션");
 		
-		//menu
+		Menu m = new Menu();
+		m.editor();
+		
+		char teamA, teamB; // 팀이름
+		int [][] inning = new int[2][12];
+		int outcount = 0;
+		
+		int iA,iB;
+		
+		for(iA = 0; iA < inning.length ; iA++){
+			for(iB = 0; iB < inning[iA].length; iB ++){
+				inning[iA][iB] = 0;
+			}
+		}
+		int inningend = 0;
+		while(inningend == 9){
+			
+			
+			
+		}
+		
+		
+		
 	}
 }
---
+++ baseball_Simul_0.01/src/Menu.java
@@ -0,0 +1,9 @@
+
+public class Menu {
+
+	public void editor(){
+		int num = 0;
+		
+		
+	}
+}
