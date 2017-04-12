a5b75e8b8daaaa994e35234f49764f84481db5fb
boy16 <boy16@DESKTOP-CDTSI5B>
2017-04-12 15:43:51 +0900

    test3

--- baseball_Simul_0.01/src/ScoreSave.java
+++ baseball_Simul_0.01/src/ScoreSave.java
@@ -1,5 +1,23 @@
 
 public class ScoreSave {
 
+	public void homeawaychange(){ //home away change
+		
+	}
+
+	public void inningcheck(){ // check inning
+		
+	}
+	
+	public void homescorecheck(){ // save hometeamscore
+		
+	}
+
+	public void awayscorecheck(){ // save awayteamscore
+		
+	}
+	
+
+	
 	
 }
--- baseball_Simul_0.01/src/Simulation.java
+++ baseball_Simul_0.01/src/Simulation.java
@@ -1,6 +1,13 @@
 
 public class Simulation {
 	
-
+	public void inninggame(){
+		
+		int onBase = 0; // base 진루상황 배열로 처리??
+		int outCount = 0; // 아웃카운트
+		int ballCountS = 0; // 스트라이크 볼카운트
+		int ballCountB = 0; // 볼 볼카운트
+		
+	}
 }
 
